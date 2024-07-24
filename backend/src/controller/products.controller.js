afedd2761d3b8faefdddd66a11e44298ddbe9a99
Franco Balich <franco.balich@gmail.com>
2024-07-24 19:38:08 -0300

    Feat: Add pool

--- backend/src/controller/products.controller.js
+++ backend/src/controller/products.controller.js
@@ -1,4 +1,16 @@
 import { response } from "express"
+import {Pool} from 'pg'
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Configuración de la conexión a la base de datos
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PWS,
+  port: process.env.DB_PORT,
+});
 
 export const getProducts = (req, res = response)=>{
   try{
