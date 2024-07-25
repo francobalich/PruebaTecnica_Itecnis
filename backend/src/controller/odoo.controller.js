93e5174237ec20bec1c1025e4668864717cd014f
Franco Balich <franco.balich@gmail.com>
2024-07-24 21:06:13 -0300

    Feat: Add odoo controller

---
+++ backend/src/controller/odoo.controller.js
@@ -0,0 +1,26 @@
+import { response } from "express"
+import {Pool} from 'pg'
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Configuración de la conexión a la base de datos
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PWS,
+  port: process.env.DB_PORT,
+});
+
+export const getData = (req, res = response)=>{
+  try{
+    return res.status(400).json({
+      status:"Ok",
+      message:"Code not implemented"
+    })
+  }
+  catch (err) {
+    console.log(err)
+    res.status(401).send('Some error happened')
+  }
+}
