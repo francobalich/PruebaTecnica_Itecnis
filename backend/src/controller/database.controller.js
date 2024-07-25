1f63071268c42dd9da003e8266c899cac70961e4
Franco Balich <franco.balich@gmail.com>
2024-07-25 13:51:07 -0300

    Feat: Add imageUrl

--- backend/src/controller/database.controller.js
+++ backend/src/controller/database.controller.js
@@ -1,15 +1,38 @@
 import mysql from 'mysql2/promise';
 import { dbConfig } from '../utils/dbConfig.js';
+import { queryProductTemplatesWithCategoryNames } from './odoo.controller.cjs';
 
 async function connectToDatabase() {
   const connection = await mysql.createConnection(dbConfig);
   return connection;
 }
 
+const readOdooData = async()=>{
+  const resp= await queryProductTemplatesWithCategoryNames()
+  let data = []
+  console.log(resp[0]);
+  resp.forEach(element => {
+    data.push(
+      {
+      ...element,
+      imageUrl:"https://random.imagecdn.app/500/500"
+    }
+    )
+  });
+  return data;
+}
+
 export async function getAllProducts() {
   const connection = await connectToDatabase()
   try {
     const [rows, fields] = await connection.execute('SELECT * FROM challengeitecnis.products');
+    if(rows.length===0){
+      const resp = await readOdooData()
+      console.log(resp);
+      /*resp.forEach(async (element) => {
+        await saveProducts(element)
+      })*/
+    }
     return rows
   } catch (err) {
     console.error('Error de consulta: ' + err.stack);
@@ -21,7 +44,7 @@ export async function getAllProducts() {
 export async function saveProducts(product) {
   const connection = await connectToDatabase()
   try {
-    const query = `INSERT INTO  challengeitecnis.products (id,title, category, imageUrl, price, products.description) VALUES ('0','${product.titulo}', '${product.categoria}', '${product.imagen}', '${product.precio}', '${product.descripcion}');`
+    const query = `INSERT INTO  challengeitecnis.products (id,title, category, imageUrl, price, products.description) VALUES ('0','${product.title}', '${product.category}', '${product.image}', '${product.price}', '${product.description}');`
     const [rows, fields] = await connection.execute(query);
     return rows
   } catch (err) {
@@ -30,3 +53,4 @@ export async function saveProducts(product) {
     await connection.end();
   }
 }
+
