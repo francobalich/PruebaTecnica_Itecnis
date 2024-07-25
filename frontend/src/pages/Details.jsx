1071f78b37e8c65a28e1d78e55596367812d6939
Franco Balich <franco.balich@gmail.com>
2024-07-25 00:53:57 -0300

    Feat: Add details page

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -1,8 +1,30 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import data from '../data/dataDemo.json'
+import './Details.css'
+import { DetailsData } from "../components/DetailsData";
+import { NumberSelect } from "../components/NumberSelect";
 
 export const Details = () => {
   const { id } = useParams();
+  const product = data[id]
   return (
-    <div>Details {id}</div>
+    <>
+      <DetailsData />
+      <section className="details">
+        <div className="details__container">
+          <img src={product.imagen} alt='producto' />
+          <div className='details__data'>
+            <h5>{product.titulo}</h5>
+            <p className='card__category' >{product.categoria}</p>
+            <p>{product.descripcion}</p>
+            <p className="details__price">${product.precio}</p>
+            <div className="details__actions">
+              <NumberSelect initialValue={1} />
+              <Link className='btnBuy' to={`/buy/${id}`}>Comprar</Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
