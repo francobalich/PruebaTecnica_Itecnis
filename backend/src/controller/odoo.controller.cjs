6f516b7e7a883bdcbd3030ce3a6a20a14aebdabe
Franco Balich <franco.balich@gmail.com>
2024-07-25 13:38:02 -0300

    Fix: Change query

--- backend/src/controller/odoo.controller.cjs
+++ backend/src/controller/odoo.controller.cjs
@@ -6,23 +6,11 @@ require('dotenv').config();
 const pool = new Pool({
   user: process.env.ODOO_USER,
   host: process.env.ODOO_HOST,
-  database: process.env.ODOO_DATABASE,
+  database: process.env.ODOO_BD,
   password: process.env.ODOO_PWS,
   port: process.env.ODOO_PORT,
 });
 
-const getData = (req, res = response) => {
-  try {
-    return res.status(400).json({
-      status: "Ok",
-      message: "Code not implemented"
-    })
-  }
-  catch (err) {
-    console.log(err)
-    res.status(401).send('Some error happened')
-  }
-}
 
 const queryProductTemplatesWithCategoryNames = async () => {
   console.log("Empezando conexion");
@@ -30,23 +18,22 @@ const queryProductTemplatesWithCategoryNames = async () => {
   console.log("Conectado");
   try {
     const queryText = `
-      SELECT pt.*, pc.name AS category_name
-      | FROM product_template pt
+      SELECT pt.name AS title,pt.list_price, pc.name AS category_name, pt.description FROM product_template pt
       LEFT JOIN product_category pc ON pt.categ_id = pc.id
       WHERE pt.categ_id IS NOT null and pt.list_price is not null
-      LIMIT 400;`;
+      LIMIT 100;`;
     const res = await client.query(queryText);
     return res.rows;
   } finally {
     client.release();
   }
 }
-queryProductTemplatesWithCategoryNames()
-  .then((result) => {
-    console.log("Resultados de la consulta:", result);
-  })
-  .catch((error) => {
-    console.error("Error en la consulta:", error);
-  });
+// queryProductTemplatesWithCategoryNames()
+//   .then((result) => {
+//     console.log("Resultados de la consulta:", result);
+//   })
+//   .catch((error) => {
+//     console.error("Error en la consulta:", error);
+//   });
 
   module.exports={queryProductTemplatesWithCategoryNames}
