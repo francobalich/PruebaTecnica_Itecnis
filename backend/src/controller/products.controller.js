de7c3934aefb680ec0fb5aba6b8efaa164a9b9c7
Franco Balich <franco.balich@gmail.com>
2024-07-25 11:22:44 -0300

    Feat: Add getProducts

--- backend/src/controller/products.controller.js
+++ backend/src/controller/products.controller.js
@@ -1,10 +1,11 @@
 import { response } from "express"
 
-export const getProducts = (req, res = response)=>{
-  try{
-    return res.status(400).json({
-      status:"Ok",
-      message:"Code not implemented"
+export const getProducts = async (req, res = response) => {
+  try {
+    const resp = await getAllProducts()
+    return res.status(200).json({
+      status: "Ok",
+      data: resp
     })
   }
   catch (err) {
@@ -13,22 +14,20 @@ export const getProducts = (req, res = response)=>{
   }
 }
 
-export const postProduct =async(req, res = response)=>{
-  try{
-    //queryProductTemplatesWithCategoryNames()
-    //const resp=await getAllProducts()
+export const postProduct = async (req, res = response) => {
+  try {
     const product = {
-        id: 11,
-        titulo: "Router TP-Link Archer AX50",
-        precio: 149.99,
-        categoria: "Redes",
-        imagen: "https://static.tp-link.com/Archer-AX50_01_normal_1616726600988r.jpg",
-        descripcion: "Router WiFi 6 con velocidad de hasta 3 Gbps, cobertura amplia y múltiples funciones de seguridad."
+      id: 11,
+      titulo: "Router TP-Link Archer AX50",
+      precio: 149.99,
+      categoria: "Redes",
+      imagen: "https://static.tp-link.com/Archer-AX50_01_normal_1616726600988r.jpg",
+      descripcion: "Router WiFi 6 con velocidad de hasta 3 Gbps, cobertura amplia y múltiples funciones de seguridad."
     }
-    const resp=await saveProducts(product)
+    const resp = await saveProducts(product)
     return res.status(200).json({
-      status:"Ok",
-      data:resp
+      status: "Ok",
+      data: resp
     })
   }
   catch (err) {
