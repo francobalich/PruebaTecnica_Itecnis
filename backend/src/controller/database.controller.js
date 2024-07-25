a5b40d0d6e260d1e6e2ac85b6a62be1e22f9d896
Franco Balich <franco.balich@gmail.com>
2024-07-25 11:21:56 -0300

    Feat: Add save products

---
+++ backend/src/controller/database.controller.js
@@ -0,0 +1,35 @@
+import mysql from 'mysql2/promise';
+import { dbConfig } from '../utils/dbConfig.js';
+
+async function connectToDatabase() {
+  const connection = await mysql.createConnection(dbConfig);
+  return connection;
+}
+
+export async function getAllProducts() {
+  const connection = await connectToDatabase()
+  try {
+    const [rows, fields] = await connection.execute('SELECT * FROM challengeitecnis.products');
+    return rows
+  } catch (err) {
+    console.error('Error de consulta: ' + err.stack);
+  } finally {
+    await connection.end();
+  }
+}
+
+export async function saveProducts(product) {
+  const connection = await connectToDatabase()
+  try {
+    const query = `INSERT INTO  challengeitecnis.products (id,title, category, imageUrl, price, products.description) VALUES ('0','${product.titulo}', '${product.categoria}', '${product.imagen}', '${product.precio}', '${product.descripcion}');`
+    console.log("----");
+    console.log(query);
+    console.log("----");
+    const [rows, fields] = await connection.execute(query);
+    return rows
+  } catch (err) {
+    console.error('Error de consulta: ' + err.stack);
+  } finally {
+    await connection.end();
+  }
+}
--- backend/src/controller/products.controller.js
+++ backend/src/controller/products.controller.js
@@ -12,3 +12,27 @@ export const getProducts = (req, res = response)=>{
     res.status(401).send('Some error happened')
   }
 }
+
+export const postProduct =async(req, res = response)=>{
+  try{
+    //queryProductTemplatesWithCategoryNames()
+    //const resp=await getAllProducts()
+    const product = {
+        id: 11,
+        titulo: "Router TP-Link Archer AX50",
+        precio: 149.99,
+        categoria: "Redes",
+        imagen: "https://static.tp-link.com/Archer-AX50_01_normal_1616726600988r.jpg",
+        descripcion: "Router WiFi 6 con velocidad de hasta 3 Gbps, cobertura amplia y múltiples funciones de seguridad."
+    }
+    const resp=await saveProducts(product)
+    return res.status(200).json({
+      status:"Ok",
+      data:resp
+    })
+  }
+  catch (err) {
+    console.log(err)
+    res.status(401).send('Some error happened')
+  }
+}
