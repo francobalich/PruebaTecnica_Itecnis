db451d2ea12678d7ed072f10dc461e278d9f496d
Franco Balich <franco.balich@gmail.com>
2024-07-25 11:21:45 -0300

    Change: Clean code

--- backend/src/controller/categories.controller.js
+++ backend/src/controller/categories.controller.js
@@ -1,12 +1,11 @@
 import { response } from "express"
-import { queryProductTemplatesWithCategoryNames } from "./odoo.controller.cjs"
 
-export const getCategories = (req, res = response)=>{
+export const getCategories =async(req, res = response)=>{
   try{
-    queryProductTemplatesWithCategoryNames()
-    return res.status(400).json({
+
+    return res.status(200).json({
       status:"Ok",
-      message:"Code not implemented"
+      data:''
     })
   }
   catch (err) {
