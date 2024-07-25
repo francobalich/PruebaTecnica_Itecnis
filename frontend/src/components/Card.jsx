6fc89aa32b30dcc5f4f041cbee1f20b0d9825630
Franco Balich <franco.balich@gmail.com>
2024-07-25 00:06:05 -0300

    Feat: Add cards dynamicly

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,14 +1,19 @@
 import pngProduct from '../assets/producto.jpg'
 import './Card.css'
 
-export const Card = () => {
+export const Card = ({
+  title="Sin titulo",
+  category="Sin categoria",
+  price="X.XX",
+  img=pngProduct
+}) => {
   return (
     <div className='card'>
-      <img src={pngProduct} alt='producto'/>
+      <img src={img} alt='producto'/>
       <div className='card__data'>
-        <h5>Titulo</h5>
-        <p className='card__category' >Categoria</p>
-        <p>$60000</p>
+        <h5>{title}</h5>
+        <p className='card__category' >{category}</p>
+        <p>${price}</p>
         <button>Comprar</button>
       </div>
     </div>
--- frontend/src/components/Catalog.jsx
+++ frontend/src/components/Catalog.jsx
@@ -1,16 +1,27 @@
+import { useEffect, useState } from 'react'
 import { Card } from './Card'
 import './Catalog.css'
+import data from '../data/dataDemo.json'
 
 export const Catalog = () => {
+  const [cards, setCards] = useState(<></>)
+  useEffect(() => {
+    setCards(() => {
+      return data.map(element => {
+        return <Card
+          title={element.titulo}
+          category={element.categoria}
+          price={element.precio}
+          img={element.imagen}
+        />
+      });
+    })
+  }, [])
+
   return (
     <section className='catalog'>
       <div className='catalog_container'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {cards}
       </div>
     </section>
   )
