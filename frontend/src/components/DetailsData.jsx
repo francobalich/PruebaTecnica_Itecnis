91b236d07a9497bc7f4a07b6e106b5d40c453fac
Franco Balich <franco.balich@gmail.com>
2024-07-25 00:56:24 -0300

    Change: Add return buton

--- frontend/src/components/DetailsData.jsx
+++ frontend/src/components/DetailsData.jsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom'
 import './Filters.css'
 
-export const DetailsData = ({ category="Sin categoria",title="Titulo" }) => {
+export const DetailsData = ({ category = "Sin categoria", title = "Titulo" }) => {
   return (
     <section className='filters'>
       <div className='filters__container'>
         <div className='filters__message'>
           <p>{category} | {title} </p>
         </div>
+        <Link className='btnReturn' to='/'>Volver</Link>
       </div>
     </section>
   )
