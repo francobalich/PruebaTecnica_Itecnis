806e979ae42f23f852fd7ce350d6956fa692d0d4
Franco Balich <franco.balich@gmail.com>
2024-07-25 12:02:03 -0300

    Change: add ProductsProvider

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,13 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { Buy, Details, Home } from "../pages"
+import { ProductsProvider } from "../context/ProductsProvider"
 
 export const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/buy/:id" element={<Buy/>} />
-      <Route path="/details/:id" element={<Details/>} />
-      <Route path="/*" element={<Navigate to='/' />} />
-    </Routes>
+    <ProductsProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buy/:id" element={<Buy />} />
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/*" element={<Navigate to='/' />} />
+      </Routes>
+    </ProductsProvider>
+
   )
 }
